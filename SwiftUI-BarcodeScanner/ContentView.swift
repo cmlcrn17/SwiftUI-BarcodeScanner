31d1cf9a710bb4a636807cc8a8b7d036a0861f45
ceren <cemilecerenerdem@gmail.com>
2020-06-02 11:55:00 +0300

    Initial Commit

---
+++ SwiftUI-BarcodeScanner/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-BarcodeScanner
+//
+//  Created by Ceren on 2.06.2020.
+//  Copyright © 2020 ceren. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
